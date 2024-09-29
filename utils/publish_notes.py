8cea4e3945149d29ec46122a442476cc945f4036
Enchantek <patrickgatpandan@gmail.com>
2024-09-29 03:40:39 -0700

    #publish search function implemented

--- utils/publish_notes.py
+++ utils/publish_notes.py
@@ -1,4 +1,5 @@
 import os
+import yaml
 
 # recursively loop through the private directory
 # copies .md files with "#publish" tag into public
@@ -8,8 +9,22 @@ public_vault = os.getenv('public_vault')
 
 private_vault_path = str(private_vault)
 
-def search_publish_hashtag(public_path, private_path):
+def search_files(public_path, private_path):
     for root, dirs, files in os.walk(public_path):
         for file in files:
             if file.endswith('.md'):
-                print(os.path.join(root, file))
+                file_path = os.path.join(root, file)
+                if has_publish_hashtag(file_path):
+                    print(file_path)
+
+def has_publish_hashtag(path):
+    with open(path, 'r', encoding='iso-8859-1') as file:
+            content = file.read()
+            parts = content.split('---', 2)
+            if len(parts) >= 3:
+                    frontmatter = parts[1].strip()
+                    metadata = yaml.safe_load(frontmatter)
+                    if 'tags' in metadata and isinstance(metadata['tags'], list):
+                        return any(str(tag).startswith('publish') for tag in metadata['tags'])
+
+search_files(private_vault, public_vault)
